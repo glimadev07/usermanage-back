a42435c08fe7f6d02000905f70c98c9ed16b05d8
glimadev07 <gilbertolimadev07@gmail.com>
2024-07-10 20:54:18 -0300

    first commit

---
+++ src/main/java/br/com/usermanage/usermanage/UsermanageBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.usermanage.usermanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermanageBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermanageBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/usermanage/usermanage/UsermanageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.usermanage.usermanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanageBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
